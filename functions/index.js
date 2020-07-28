5b1dbdf04b4fb91652f79277fb617939dd1729a3
Rohan Kapur <rohankapurdev@outlook.com>
2020-07-29 00:10:22 +0800

    Added routes

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,14 @@
-const functions = require('firebase-functions');
-const app = require('express')();  // Import and initialize express
+const functions = require("firebase-functions");
+const app = require("express")(); // Import and initialize express
 
-exports.api = functions.https.onRequest(app);  // Serve the express routes via the /api endpoint on the main URI
+// Main Routes
+app.post("/outputData", getOutputDataForWallet);
+app.post("/txData", getTransactionDataForWallet);
+app.post("/pushtx", pushTransaction);
+
+// Miscellaneous Routes
+app.post("/bitcoinPrice", getBitcoinPrice);
+app.get("/fees", getOnChainFeeDensity);
+app.post("/txCount", getTransactionCountForAddress);
+
+exports.api = functions.https.onRequest(app); // Serve the express routes via the /api endpoint on the main URI
