8707e06b7538c50cd6c0a96e6971edf01fd17a42
Rohan Kapur <rohankapurdev@outlook.com>
2020-08-03 23:13:19 +0800

    Added push tx handler and logic

---
+++ functions/handlers/push-tx.handler.js
@@ -0,0 +1,21 @@
+const axios = require("axios").default;
+
+exports.pushTransaction = async (req, res) => {
+  const hex = req.body.hex;
+  const url = req.body.url;
+
+  return res
+    .json(await sendHexToNetwork(hex, url))
+    .catch((err) => console.error(err));
+};
+
+// Handler logic
+
+sendHexToNetwork = async (hex, url) => {
+  const res = await axios
+    .post(`${url}/tx`, hex)
+    .then((response) => response.data)
+    .catch((err) => console.error(err));
+
+  return res;
+};
