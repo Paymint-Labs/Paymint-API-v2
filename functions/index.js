f78b8aa269fcf37c664153ea743740aa1fdb0cc4
Rohan Kapur <rohankapurdev@outlook.com>
2020-08-03 23:12:22 +0800

    Added primary and misc routes

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,27 @@
 const functions = require("firebase-functions");
 const app = require("express")(); // Import and initialize express
 
-const { getOutputDataForWallet } = require('./handlers/output-data.handler');
+const { getFeeDensity } = require("./handlers/fees.handler");
+const { pushTransaction } = require("./handlers/push-tx.handler");
+const { getTxCountForAddress } = require("./handlers/tx-count.handler");
+const { getAddressForIndexTx } = require("./handlers/voutlookup.handler");
+const { getOutputDataForWallet } = require("./handlers/output-data.handler");
+const { getTransactionDataForWallet } = require("./handlers/tx-data.handler");
+const {
+  getBitcoinPrice,
+  getHistorialPrice,
+} = require("./handlers/bitcoin-price.handler");
 
 // Main Routes
 app.post("/outputData", getOutputDataForWallet);
 app.post("/txData", getTransactionDataForWallet);
+app.post("/voutLookup", getAddressForIndexTx);
 app.post("/pushtx", pushTransaction);
 
 // Miscellaneous Routes
-app.post("/bitcoinPrice", getBitcoinPrice);
-app.get("/fees", getOnChainFeeDensity);
-app.post("/txCount", getTransactionCountForAddress);
+app.post("/currentBitcoinPrice", getBitcoinPrice);
+app.post("/historicalBitcoinPrice", getHistorialPrice);
+app.get("/fees", getFeeDensity);
+app.post("/txCount", getTxCountForAddress);
 
 exports.api = functions.https.onRequest(app); // Serve the express routes via the /api endpoint on the main URI
