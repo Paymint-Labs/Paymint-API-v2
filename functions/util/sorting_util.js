7bdb1bdbc207b6c7773fd2470b9c9c406bbfbea8
Rohan Kapur <rohankapurdev@outlook.com>
2020-08-03 23:14:40 +0800

    Added sorting utilities

--- functions/util/sorting_util.js
+++ functions/util/sorting_util.js
@@ -1,16 +1,22 @@
 // The following functions are sorting utility/helper functions to be used wherever necessary
+const { extractDateFromTimestamp, convertToDisplayPrice } = require("./misc");
+const {
+  currentPriceLookup,
+  historicPriceFetch,
+} = require("../handlers/bitcoin-price.handler");
+const functions = require("firebase-functions");
 
 /**
  * Returns true if the transaction id, [idToCheckFor] exists in the
  * transaction array passed to the function, [txArrayToCheckFrom]
+ * Returns false otherwise.
  *
- * Returns false otherwise
  * @param {array} txArrayToCheckFrom
  * @param {string} idToCheckFor
  */
 exports.duplicateTransactionCheck = (txArrayToCheckFrom, idToCheckFor) => {
   for (let i = 0; i < txArrayToCheckFrom.length; i++) {
-    if (txArrayToCheckFrom[i].txid == idToCheckFor) {
+    if (txArrayToCheckFrom[i].txid === idToCheckFor) {
       return true;
     }
   }
@@ -18,22 +24,23 @@ exports.duplicateTransactionCheck = (txArrayToCheckFrom, idToCheckFor) => {
 };
 
 /**
- * Expects the response from sortTransactionArray as input [txArray]. Returns
+ * Expects the response from sortTransactionArray as input, [txArray]. Returns
  * the expected output of /txData
+ *
  * @param {array} txArray
  */
 exports.buildDatetimeChunks = (txArray) => {
   let finalDictionary = { dateTimeChunks: [] };
   let dateArray = [];
-  let orderedList = [];
 
-  txArray.forEach((txObject) => {
-    let timestampDate = this.extractDateFromTimestamp(txObject.timestamp);
+  for (let i = 0; i < txArray.length; i++) {
+    const txObject = txArray[i];
+    let timestampDate = extractDateFromTimestamp(txObject.timestamp);
     let txTimeArray = [txObject.timestamp, timestampDate];
 
     if (dateArray.includes(txTimeArray[1])) {
       finalDictionary.dateTimeChunks.forEach((chunk) => {
-        if (this.extractDateFromTimestamp(chunk.timestamp) == txTimeArray[1]) {
+        if (extractDateFromTimestamp(chunk.timestamp) === txTimeArray[1]) {
           chunk["transactions"].push(txObject);
         }
       });
@@ -45,40 +52,32 @@ exports.buildDatetimeChunks = (txArray) => {
       };
       finalDictionary.dateTimeChunks.push(timeChunkObject);
     }
-  });
-
-  finalDictionary.dateTimeChunks.forEach((dateTimeChunk) => {
-    if (!dateTimeChunk.timestamp) {
-      orderedList.push(dateTimeChunk);
-    }
-  });
-
-  finalDictionary.dateTimeChunks.forEach((dateTimeChunk) => {
-    if (dateTimeChunk.timestamp) {
-      orderedList.push(dateTimeChunk);
-    }
-  });
-
-  return { dateTimeChunks: orderedList };
+  }
+  return finalDictionary;
 };
 
 /**
  * Returns something - fill this in later
+ *
  * @param {array} txArray
- * @param {string[]} internalAndChangeAddrArray
+ * @param {string[]} allAddresses
  * @param {string} currency
  * @param {string[]} changeAddresses
  */
 exports.sortTransactionArray = async (
   txArray,
-  internalAndChangeAddrArray,
+  allAddresses,
   currency,
   changeAddresses
 ) => {
+  const cryptocompareKey = functions.config().cryptocompare.key;
+  // const cryptocompareKey =
+  //   "650ac229320ae8d475fce6df4650fd8a05f77e591060d2e5603ea10294489250";
   let midSortedTxArray = [];
-  let currentPrice = await this.getBitcoinPrice(currency);
+  let currentPrice = await currentPriceLookup(currency, cryptocompareKey);
 
-  txArray.forEach(async (txObject) => {
+  for (let i = 0; i < txArray.length; i++) {
+    const txObject = txArray[i];
     let sendersArray = [];
     let recipientsArray = [];
     let inputAmtSentFromWallet = 0; // Usually only has value when txType = 'Send'
@@ -98,18 +97,14 @@ exports.sortTransactionArray = async (
     });
 
     // Checks for user controlled addresses in each transaction's input array and marks tyType accordingly
-    const foundInSenders = internalAndChangeAddrArray.some((address) =>
+    const foundInSenders = allAddresses.some((address) =>
       sendersArray.includes(address)
     );
 
     // If txType = Sent, then calulate inputAmtSentFromWallet, calculate who received how much in aliens array (check outputs)
     if (foundInSenders) {
       txObject.vin.forEach((input) => {
-        if (
-          internalAndChangeAddrArray.includes(
-            input.prevout.scriptpubkey_address
-          )
-        ) {
+        if (allAddresses.includes(input.prevout.scriptpubkey_address)) {
           inputAmtSentFromWallet += input.prevout.value;
         }
       });
@@ -120,10 +115,10 @@ exports.sortTransactionArray = async (
       });
       // Subtract transaction fee
       inputAmtSentFromWallet -= txObject.fee;
-      // If txType = Received, then calculate output amount addressed to user controlled addresses - (internalAndChangeAddrArray)
+      // If txType = Received, then calculate output amount addressed to user controlled addresses - (allAddresses)
     } else if (!foundInSenders) {
       txObject.vout.forEach((output) => {
-        if (internalAndChangeAddrArray.includes(output.scriptpubkey_address)) {
+        if (allAddresses.includes(output.scriptpubkey_address)) {
           outputAmtAddressedToWallet += output.value;
         }
       });
@@ -137,12 +132,12 @@ exports.sortTransactionArray = async (
       midSortedTx.txType = "Sent";
       midSortedTx.amount = inputAmtSentFromWallet;
       let worthNowRaw = currentPrice * (inputAmtSentFromWallet / 100000000);
-      midSortedTx.worthNow = this.convertToDisplayPrice(worthNowRaw, currency);
+      midSortedTx.worthNow = convertToDisplayPrice(worthNowRaw, currency);
     } else {
       midSortedTx.txType = "Received";
       midSortedTx.amount = outputAmtAddressedToWallet;
       let worthNowRaw = currentPrice * (outputAmtAddressedToWallet / 100000000);
-      midSortedTx.worthNow = this.convertToDisplayPrice(worthNowRaw, currency);
+      midSortedTx.worthNow = convertToDisplayPrice(worthNowRaw, currency);
     }
     midSortedTx.aliens = aliens; // Recepients in a 'Send' txType transaction
     midSortedTx.fees = txObject.fee;
@@ -152,22 +147,33 @@ exports.sortTransactionArray = async (
     midSortedTx.outputs = txObject.vout;
 
     midSortedTxArray.push(midSortedTx);
-  });
+  }
 
   // Loop through midSortedTxArray and add worthAtBlockTimestamp property to every object
   // Cannot use foreach because of async-await restrictions, use normal for instead
   for (let i = 0; i < midSortedTxArray.length; i++) {
     let transaction = midSortedTxArray[i];
-    let priceAtBlockTimestamp = await this.getBitcoinPrice(
-      currency,
-      transaction.timestamp
-    );
+
+    let priceAtBlockTimestamp = await historicPriceFetch(
+      transaction.timestamp,
+      cryptocompareKey
+    )[currency];
+
     let worthAtBlockTimestampRaw =
       priceAtBlockTimestamp * (transaction.amount / 100000000);
-    transaction.worthAtBlockTimestamp = this.convertToDisplayPrice(
+
+    transaction.worthAtBlockTimestamp = convertToDisplayPrice(
       worthAtBlockTimestampRaw,
       currency
     );
+
+    if (
+      transaction.worthAtBlockTimestamp.substr(
+        transaction.worthAtBlockTimestamp.length - 3
+      ) === "NaN"
+    ) {
+      transaction.worthAtBlockTimestamp = "---";
+    }
   }
 
   // Return a descending order version of midSortedTxArray
